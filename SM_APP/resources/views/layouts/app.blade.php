39371a67121d2ff2252282e884465ba78fcc06b7
Arafatul islam <arafatulislamtc@gmail.com>
2024-02-06 00:24:33 +0600

    make active classes,html theme clean ,,higher-header title ,,remove useless features for this project

--- SM_APP/resources/views/layouts/app.blade.php
+++ SM_APP/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>{{ !empty($header_title) ? $header_title : ''}}-School</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -25,15 +25,10 @@
   <link rel="stylesheet" href="{{ url('plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
   <link rel="stylesheet" href="{{ url('plugins/summernote/summernote-bs4.min.css') }}">
+  @yield('style')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
-
-  <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ url('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
-  </div>
-
 
     {{-- navbar --}}
   @include('layouts.header')
@@ -84,5 +79,6 @@
 <script src="{{ url('dist/js/demo.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ url('dist/js/pages/dashboard.js') }}"></script>
+@yield('script')
 </body>
 </html>
